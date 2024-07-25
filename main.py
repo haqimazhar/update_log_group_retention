6bdb568996a063463ef8d264225778865b946c94
Haqim Azhar <luqman.h@rocketbots.io>
2024-07-25 23:52:20 +0800

    chore: update code to check for log group name and rentention period

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import boto3
 
-import boto3
-
 def get_all_log_groups():
     client = boto3.client('logs')
     log_groups = []
@@ -10,7 +8,7 @@ def get_all_log_groups():
     for page in paginator.paginate():
         log_groups.extend(page['logGroups'])
     
-    return [lg['logGroupName'] for lg in log_groups]
+    return log_groups
 
 def update_log_group_retention(log_group_name, retention_days=7):
     client = boto3.client('logs')
@@ -25,7 +23,10 @@ def update_log_group_retention(log_group_name, retention_days=7):
 
 if __name__ == "__main__":
     log_groups = get_all_log_groups()
-    print(log_groups)
     
-    for log_group_name in log_groups:
-        update_log_group_retention(log_group_name)
+    for lg in log_groups:
+        log_group_name = lg['logGroupName']
+        retention_in_days = lg.get('retentionInDays')
+        
+        if (log_group_name.startswith('/aws/lambda/') or log_group_name.startswith('/aws/kinesis/')) and retention_in_days is None:
+            update_log_group_retention(log_group_name)
